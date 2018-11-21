3587bcec3a6c007a12b80ed03d404ead954e5cc2
github username <github email address>
2018-11-21 09:43:15 +0000

    read me

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import logo from './images/bookshell-logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+    <React.Fragment>
+      <CssBaseline />
       <div className="App">
         <header className="App-header">
+        <div>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Bookshell</h1>
+          </div>
         </header>
       </div>
+      </React.Fragment>
     );
   }
 }
